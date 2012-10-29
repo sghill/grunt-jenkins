b76713f75c0914e329651209610270b4a809678c
sghill <sg@sghill.net>
2012-10-28 23:36:47 -0700

    downloading config.xml for all jobs on a hardcoded server

--- tasks/jenkins.js
+++ tasks/jenkins.js
@@ -1,3 +1,6 @@
+var request = require('request');
+var fs = require('fs');
+var _ = require('underscore');
 /*
  * grunt-jenkins
  * https://github.com/sghill/grunt-jenkins
@@ -15,16 +18,68 @@ module.exports = function(grunt) {
   // TASKS
   // ==========================================================================
 
-  grunt.registerTask('jenkins', 'Your task description goes here.', function() {
-    grunt.log.write(grunt.helper('jenkins'));
+  function logJobName(job) {
+    grunt.log.writeln(job.name);
+  }
+
+  grunt.registerTask('jenkins-list-projects', 'list projects on jenkins server', function() {
+    var done = this.async();
+    grunt.helper('getProjectNames', logJobName, done);
+ });
+
+  grunt.registerTask('jenkins-backup-configs', 'backup listed projects on jenkins server', function() {
+    var done = this.async();
+    request('http://192.168.241.137/api/json', function(e, r, body) {
+      var isSuccess = !e && r.statusCode === 200;
+      if(isSuccess) {
+        var jobs = JSON.parse(body).jobs;
+
+        _.each(jobs, function(job) {
+          var jobConfigurationUrl = [job.url, 'config.xml'].join('/');
+
+          request(jobConfigurationUrl, function(e, r, body) {
+            var pipelineDirectory = 'pipeline';
+            var directoryName = [pipelineDirectory, job.name].join('/');
+            var filename = [pipelineDirectory, job.name, 'config.xml'].join('/');
+
+            if(!fs.existsSync(pipelineDirectory)) {
+              fs.mkdirSync(pipelineDirectory); 
+            }
+
+            if(!fs.existsSync(directoryName)) { 
+              fs.mkdirSync(directoryName); 
+            }
+
+            fs.writeFile(filename, body, 'utf8', function(err) {
+              if(err) { throw err; }
+
+              grunt.log.writeln('created file: ' + filename);
+              if(_.isEqual(job, _.last(jobs))) {
+                done(isSuccess);
+              }
+            });
+
+          });
+        });
+
+      }
+    });
   });
+
 
   // ==========================================================================
   // HELPERS
   // ==========================================================================
 
-  grunt.registerHelper('jenkins', function() {
-    return 'jenkins!!!';
+  grunt.registerHelper('getProjectNames', function(callback, done) {
+    request('http://192.168.241.137/api/json', function(e, r, body) {
+      var isSuccess = !e & r.statusCode === 200;
+      if(isSuccess) {
+        var jobs = JSON.parse(body).jobs;
+        jobs.forEach(callback);
+      }
+      done(isSuccess);
+    });
   });
 
 };
