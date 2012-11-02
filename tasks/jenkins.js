9a6a34a26d27f54df51bbeb5916b66d49aec1035
sghill <sg@sghill.net>
2012-11-01 23:48:17 -0700

    verifying jobs

--- tasks/jenkins.js
+++ tasks/jenkins.js
@@ -261,20 +261,38 @@ module.exports = function(grunt) {
     return deferred.promise;
   }
   
-  function compareNumberOfJobs(onDiskJobs) {
-    return fetchJobsFromServer().
-      then(function(serverJobs) {
-        if(onDiskJobs.length !== serverJobs.length) {
-          grunt.log.error('mismatch between jobs stored locally (' + 
-            onDiskJobs.length + ') and jobs on server (' + serverJobs.length + ')');
-        }
-      })
-  }
-  
-  function compareToJobsOnDisk(jobs) {
+  function compareToJobsOnDisk(serverJobsAndConfigurations) {
     var deferred = q.defer();
     loadJobsFromDisk().
-      then(compareNumberOfJobs)
+      then(function(jobNames) {
+        var serverJobNames = _.pluck(serverJobsAndConfigurations, 'name');
+        if(!_.isEmpty(_.difference(serverJobNames, jobNames))) {
+          grunt.log.error('Jobs mismatch.');
+          return deferred.resolve(false);
+        }
+        var results = [];
+        var errors = [];
+        _.each(jobNames, function(name) {
+          var filename = [PIPELINE_DIRECTORY, name, 'config.xml'].join('/');
+          fs.readFile(filename, function(e, contents) {
+            if(e) { errors.push(e); }
+            var serverJob = _.find(serverJobsAndConfigurations, function(j) { return j.name === name; });
+            if(serverJob) { //sometimes this comes back with nothing, figure out what the deal is
+              results.push(serverJob.config === contents.toString());
+              if(isLast(name, jobNames)) {
+                var result = _.all(results, function(r) { return r; }) && 
+                            !_.any(errors, function(e) { return e; });
+                if(result) {
+                  grunt.log.ok('All ' + jobNames.length + ' jobs verified!');
+                } else {
+                  grunt.log.error('Jobs configuration mismatch.');
+                }
+                deferred.resolve(result);
+              }
+            }
+          })
+        });
+      });
     return deferred.promise;
   }
   
@@ -300,6 +318,7 @@ module.exports = function(grunt) {
   grunt.registerTask('jenkins-install', 'jenkins-install-jobs jenkins-install-plugins');
   grunt.registerTask('jenkins-backup', 'jenkins-backup-jobs jenkins-backup-plugins');
   grunt.registerTask('jenkins-list', 'jenkins-list-jobs jenkins-list-plugins');
+  grunt.registerTask('jenkins-verify', 'jenkins-verify-jobs jenkins-verify-plugins');
 
   grunt.registerTask('jenkins-install-jobs', 'install all Jenkins jobs', function() {
     var done = this.async();
@@ -358,13 +377,14 @@ module.exports = function(grunt) {
     fetchJobsFromServer().
       then(fetchJobConfigurations).
       then(compareToJobsOnDisk).
-      then(function() { done(true); });
+      then(function(result) { done(result); }, logError);
   });
 
+  // if no plugins, this has no output at all. it should fail
   grunt.registerTask('jenkins-verify-plugins', 'verify plugins in Jenkins match the on-disk versions', function() {
     var done = this.async();
     fetchEnabledPluginsFromServer().
       then(compareToPluginsOnDisk).
-      then(function(result) { done(result); });
+      then(function(result) { done(result); }, logError);
   });
 };
