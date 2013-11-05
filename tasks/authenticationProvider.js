48f3e00b19558c1131a16de0b062819267b78ddd
sghill <sghill.dev@gmail.com>
2013-11-05 10:53:09 -0500

    don't manually default netrc to ~/.netrc, since lib does that

--- tasks/authenticationProvider.js
+++ tasks/authenticationProvider.js
@@ -1,13 +1,13 @@
 function AuthenticationProvider(grunt, netrcFactory) {
   var username      = grunt.config('jenkins.username')
     , password      = grunt.config('jenkins.password')
-    , netrcLocation = grunt.config('jenkins.netrcLocation') || '~/.netrc'
+    , netrcLocation = grunt.config('jenkins.netrcLocation')
     , netrcMachine  = grunt.config('jenkins.netrcMachine');
 
   this.get = function() {
     var netrc = netrcFactory(netrcLocation)[netrcMachine];
     if(netrc && netrc.login && netrc.password) {
-      grunt.log.ok('Using .netrc credentials from ' + netrcLocation);
+      grunt.log.ok('Using .netrc credentials from ' + (netrcLocation || '~/.netrc'));
       return {auth:{username: netrc.login, password: netrc.password}};
     }
     if(username && password) {
