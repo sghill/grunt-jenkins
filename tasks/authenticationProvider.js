76e42ae3fc12e0ffc072f14a8f978b04e804d66a
sghill <sghill.dev@gmail.com>
2013-11-05 10:27:09 -0500

    logging which authentication provider is being loaded

--- tasks/authenticationProvider.js
+++ tasks/authenticationProvider.js
@@ -1,17 +1,20 @@
-function AuthenticationProvider(grunt, netrcConstructor) {
+function AuthenticationProvider(grunt, netrcFactory) {
   var username      = grunt.config('jenkins.username')
     , password      = grunt.config('jenkins.password')
     , netrcLocation = grunt.config('jenkins.netrcLocation') || '~/.netrc'
     , netrcMachine  = grunt.config('jenkins.netrcMachine');
 
   this.get = function() {
-    var netrc = netrcConstructor(netrcLocation)[netrcMachine];
+    var netrc = netrcFactory(netrcLocation)[netrcMachine];
     if(netrc && netrc.login && netrc.password) {
+      grunt.log.ok('Using .netrc credentials from ' + netrcLocation);
       return {auth:{username: netrc.login, password: netrc.password}};
     }
     if(username && password) {
+      grunt.log.ok('Using provided username and password');
       return {auth:{username: username, password: password}};
     }
+    grunt.log.ok('Using no authentication');
     return {};
   };
 }
