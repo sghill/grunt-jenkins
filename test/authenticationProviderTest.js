c3a37aec047ff074bf3c24608602cf987b0d6e25
sghill <sghill.dev@gmail.com>
2013-11-05 11:35:59 -0500

    inject a mock grunt log... note: need mocking lib

--- test/authenticationProviderTest.js
+++ test/authenticationProviderTest.js
@@ -3,13 +3,17 @@ var AuthenticationProvider = require('../tasks/authenticationProvider.js');
 
 describe('AuthenticationProvider', function() {
 
-  var emptyNetRcMock = function() { return {}; };
+  var emptyNetRcMock = function() { return {}; }
+    , emptyLog = { ok: function() {} }
+    , emptyGruntMock = {
+        config: function() {}
+      , log: emptyLog
+      }
 
   describe('no authentication', function() {
 
     it('should return an empty object', function() {
-      var gruntMock = {config:function(){}};
-      var authenticationProvider = new AuthenticationProvider(gruntMock, emptyNetRcMock);
+      var authenticationProvider = new AuthenticationProvider(emptyGruntMock, emptyNetRcMock);
       var authenticationOptions = authenticationProvider.get();
       authenticationOptions.should.eql({});
     });
@@ -22,44 +26,47 @@ describe('AuthenticationProvider', function() {
       var gruntMock = {
         config: function(x) {
           if(x === 'jenkins.username') {
-            return 'un'; 
+            return 'un';
           }
           if(x === 'jenkins.password') {
             return 'pw';
           }
-        }
+        },
+        log: emptyLog
       };
       var provider = new AuthenticationProvider(gruntMock, emptyNetRcMock);
       var options = provider.get();
       options.should.eql({auth:{username: 'un', password: 'pw'}});
     });
-    
+
     it('should return empty if only username specified', function() {
       var gruntMock = {
         config: function(x) {
           if(x === 'jenkins.username') {
-            return 'un'; 
+            return 'un';
           }
-        }
+        },
+        log: emptyLog
       };
       var provider = new AuthenticationProvider(gruntMock, emptyNetRcMock);
       var options = provider.get();
       options.should.eql({});
     });
- 
+
     it('should return empty if only password specified', function() {
       var gruntMock = {
         config: function(x) {
           if(x === 'jenkins.password') {
             return 'pw';
           }
-        }
+        },
+        log: emptyLog
       };
       var provider = new AuthenticationProvider(gruntMock, emptyNetRcMock);
       var options = provider.get();
       options.should.eql({});
     });
-    
+
   });
 
   describe('netrc present', function() {
@@ -79,9 +86,10 @@ describe('AuthenticationProvider', function() {
           if(x === 'jenkins.netrcMachine') {
             return 'testjenkins';
           }
-        }
+        },
+        log: emptyLog
       };
-      var netRcMock = function(filepath) { 
+      var netRcMock = function(filepath) {
         if (filepath === 'netrc') {
           return {testjenkins: {login: 'un-netrc', password: 'pw-netrc'}};
         }
@@ -93,7 +101,14 @@ describe('AuthenticationProvider', function() {
     });
 
     it('should return empty if only username specified', function() {
-      var gruntMock = { config: function(x) { if(x === 'jenkins.netrcMachine') { return 'testjenkins'; } } };
+      var gruntMock = {
+        config: function(x) {
+          if(x === 'jenkins.netrcMachine') {
+            return 'testjenkins';
+          }
+        },
+        log: emptyLog
+      };
       var netRcMock = function() { return { testjenkins: { login: 'un', password: '' } } };
       var provider = new AuthenticationProvider(gruntMock, netRcMock);
       var options = provider.get();
@@ -101,7 +116,14 @@ describe('AuthenticationProvider', function() {
     });
 
     it('should return empty if only password specified', function() {
-      var gruntMock = { config: function(x) { if(x === 'jenkins.netrcMachine') { return 'testjenkins'; } } };
+      var gruntMock = {
+        config: function(x) {
+          if(x === 'jenkins.netrcMachine') {
+            return 'testjenkins';
+          }
+        },
+        log: emptyLog
+      };
       var netRcMock = function() { return { testjenkins: { login: '', password: 'pw' } } };
       var provider = new AuthenticationProvider(gruntMock, netRcMock);
       var options = provider.get();
