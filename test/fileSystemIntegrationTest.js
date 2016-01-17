934767c06dd997419e724859b7ac27b66e47a322
Steve Hill <sghill.dev@gmail.com>
2016-01-17 00:15:42 -0800

    bump shouldjs to 8.1.1 and use the promises-style assertions

--- test/fileSystemIntegrationTest.js
+++ test/fileSystemIntegrationTest.js
@@ -3,107 +3,74 @@ var should = require('should'),
   FileSystem = require('../tasks/fileSystem.js');
 
 describe('FileSystem', function() {
+  var dir = path.join(__dirname, 'support');
   var fileSystem;
 
+  function jobNamed(x) {
+    return path.join(dir, x);
+  }
+
   before(function() {
-    fileSystem = new FileSystem(path.join(__dirname, 'support'), {});
+    fileSystem = new FileSystem(dir, {});
   });
 
   describe('#loadJobs()', function() {
-    var jobsPromise;
+    var jobs;
+
     before(function() {
-      jobsPromise = fileSystem.loadJobs();
+      jobs = fileSystem.loadJobs();
     });
 
-    it('should handle spaces', function(done) {
-      jobsPromise.then(function(names) {
-        names.should.containDeep(['job 3']);
-        done();
-      });
+    it('should handle spaces', function() {
+      return jobs.should.eventually.containDeep([jobNamed('job 3')]);
     });
 
-    it('should handle hyphens', function(done) {
-      jobsPromise.then(function(names) {
-        names.should.containDeep(['job-1']);
-        done();
-      });
+    it('should handle hyphens', function() {
+      return jobs.should.eventually.containDeep([jobNamed('job-1')]);
     });
 
-    it('should handle commas', function(done) {
-      jobsPromise.then(function(names) {
-        names.should.containDeep(['job,12']);
-        done();
-      });
+    it('should handle commas', function() {
+      return jobs.should.eventually.containDeep([jobNamed('job,12')]);
     });
 
-    it('should handle dots', function(done) {
-      jobsPromise.then(function(names) {
-        names.should.containDeep(['job.2']);
-        done();
-      });
+    it('should handle dots', function() {
+      return jobs.should.eventually.containDeep([jobNamed('job.2')]);
     });
 
-    it('should handle quotes', function(done) {
-      jobsPromise.then(function(names) {
-        names.should.containDeep(['job"8']);
-        done();
-      });
+    it('should handle quotes', function() {
+      return jobs.should.eventually.containDeep([jobNamed('job"8')]);
     });
 
-    it('should handle parens', function(done) {
-      jobsPromise.then(function(names) {
-        names.should.containDeep(['job(5)']);
-        done();
-      });
+    it('should handle parens', function() {
+      return jobs.should.eventually.containDeep([jobNamed('job(5)')]);
     });
 
-    it('should handle curly braces', function(done) {
-      jobsPromise.then(function(names) {
-        names.should.containDeep(['job{11}']);
-        done();
-      });
+    it('should handle curly braces', function() {
+      return jobs.should.eventually.containDeep([jobNamed('job{11}')]);
     });
 
-    it('should handle backticks', function(done) {
-      jobsPromise.then(function(names) {
-        names.should.containDeep(['job`7']);
-        done();
-      });
+    it('should handle backticks', function() {
+      return jobs.should.eventually.containDeep([jobNamed('job`7')]);
     });
 
-    it('should handle pluses', function(done) {
-      jobsPromise.then(function(names) {
-        names.should.containDeep(['job+10']);
-        done();
-      });
+    it('should handle pluses', function() {
+      return jobs.should.eventually.containDeep([jobNamed('job+10')]);
     });
 
-    it('should handle equals', function(done) {
-      jobsPromise.then(function(names) {
-        names.should.containDeep(['job=9']);
-        done();
-      });
+    it('should handle equals', function() {
+      return jobs.should.eventually.containDeep([jobNamed('job=9')]);
     });
 
-    it('should handle tildes', function(done) {
-      jobsPromise.then(function(names) {
-        names.should.containDeep(['job~6']);
-        done();
-      });
+    it('should handle tildes', function() {
+      return jobs.should.eventually.containDeep([jobNamed('job~6')]);
     });
 
-    it('should handle unicode characters', function(done) {
-      jobsPromise.then(function(names) {
-        names.should.containDeep(['job☃4', 'jobø13']);
-        done();
-      });
+    it('should handle unicode characters', function() {
+      return jobs.should.eventually.containDeep([jobNamed('job☃4'), jobNamed('jobø13')]);
     });
 
-    it('should handle really long names', function(done) {
-      jobsPromise.then(function(names) {
-        names.should.containDeep(['this-is-a-really-long-job-name-probably-longer-than-anyone-should-name-one-because-all-the-relevant-info-gets-lost-in-the-noise-of-it-all-but-jenkins-pretty-much-always-ends-up-with-some-hideous-naming-schemes-to-compensate-for-its-lack-of-pipelines']);
-        done();
-      });
+    it('should handle really long names', function() {
+      return jobs.should.eventually.containDeep([jobNamed('this-is-a-really-long-job-name-probably-longer-than-anyone-should-name-one-because-all-the-relevant-info-gets-lost-in-the-noise-of-it-all-but-jenkins-pretty-much-always-ends-up-with-some-hideous-naming-schemes-to-compensate-for-its-lack-of-pipelines')]);
     });
 
   });
